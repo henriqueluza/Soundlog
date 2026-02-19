e3ed4912ad86f01fab4fdff4a3c13ae81a4c5c4e
Henrique Luza <henriqueluza@gmail.com>
2026-02-19 13:07:20 -0300

    Adiciona middleware com CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,3 +7,11 @@ app = FastAPI(
     version="0.1.0",
     description="API do Soundlog - plataforma social de música"
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
