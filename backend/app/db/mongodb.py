5133a2c69a7dfad4d76b3cf468a634a2f4404e04
Henrique Luza <henriqueluza@gmail.com>
2026-02-19 13:48:10 -0300

    Cria funções assíncronas close_db e connect_db

--- backend/app/db/mongodb.py
+++ backend/app/db/mongodb.py
@@ -1,1 +1,22 @@
 import motor.motor_asyncio
+import os
+
+url = os.getenv("MONGODB_URL")
+name = os.getenv("MONGO_DB_NAME")
+
+client = motor.motor_asyncio.AsyncIOMotorClient(url)
+
+db = client[name]
+
+async def connect_db():
+    try:
+        print("Conectado ao MongoDB")
+    except Exception as e:
+        print(f"Erro ao conectar: {e}")
+
+async def close_db():
+    try:
+        client.close()
+        print("Conexão com MongoDB encerrada")
+    except Exception as e:
+        print(f"Erro ao desconectar: {e}")
