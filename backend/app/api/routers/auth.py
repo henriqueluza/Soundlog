14892f49464ecaafffc34f0a9a53841017024a8e
Henrique Luza <henriqueluza@gmail.com>
2026-02-19 15:56:37 -0300

    Cria rota /register para cadastro de usuários

--- backend/app/api/routers/auth.py
+++ backend/app/api/routers/auth.py
@@ -1,5 +1,36 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from app.models.user import UserResponse, UserCreate, UserInDB
 from app.db.mongodb import db
 from app.utils import hash_password
+from datetime import datetime
 
+router = APIRouter(prefix="/auth", tags=["auth"])
+
+@router.post("/register")
+async def register(user: UserCreate):
+
+    existing_user = await db["users"].find_one({"username": user.username})
+    existing_email = await db["users"].find_one({"email": user.email})
+
+    if existing_user is not None or existing_email is not None:
+        raise HTTPException(status_code=400, detail="Usuário já existe")
+
+    hashed_password = hash_password(user.password)
+
+    new_user = UserInDB(
+        username=user.username,
+        email=user.email,
+        password_hash=hashed_password,
+        created_at=datetime.now()
+    )
+
+    await db["users"].insert_one(new_user.model_dump())
+
+    return UserResponse(
+        username=new_user.username,
+        email=new_user.email,
+        followers=new_user.followers,
+        following=new_user.following,
+        favorite_genres=new_user.favorite_genres,
+        created_at=new_user.created_at
+    )
