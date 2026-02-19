c969e7f661eae5985e5991dbdd5a225e472f1ac0
Henrique Luza <henriqueluza@gmail.com>
2026-02-19 13:04:40 -0300

    Cria app com método FastAPI

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,9 @@
 import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title="soundlog",
+    version="0.1.0",
+    description="API do Soundlog - plataforma social de música"
+)
