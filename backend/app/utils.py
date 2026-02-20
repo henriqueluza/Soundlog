42fbeee49a5dcd45beb152334d10da2b1c20267f
Henrique Luza <henriqueluza@gmail.com>
2026-02-20 09:52:23 -0300

    Cria função create_access_token para criar tokens

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -1,4 +1,13 @@
 from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime, timedelta
+import os
+from dotenv import load_dotenv
+
+load_dotenv("/Users/henriqueluza/Coding/Projeto Soundlog/.env")
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
 
 pwd_context = CryptContext(
     schemes=["bcrypt"],
@@ -11,3 +20,8 @@ def hash_password(password: str) -> str:
 
 def verify_password(password: str, hashed_password: str) -> bool:
     return pwd_context.verify(password, hashed_password)
+
+def create_access_token(data: dict):
+    expire = datetime.utcnow() + timedelta(minutes=30)
+    data["exp"] = expire
+    return jwt.encode(data, key = SECRET_KEY, algorithm=ALGORITHM)
