14be5c7478d0243711a19dffe3023cff76383928
Henrique Luza <henriqueluza@gmail.com>
2026-02-20 13:02:28 -0300

    Cria rota para atualizar dados do perfil

--- backend/app/api/routers/users.py
+++ backend/app/api/routers/users.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException
 from app.db.mongodb import db
 from app.models.user import UserResponse, UserPublicResponse
-from app.utils import get_current_user, hash_password, verify_password
+from app.utils import get_current_user, hash_password, verify_password, UserUpdate
+from fastapi.security import OAuth2PasswordBearer
+
+from backend.app.utils import oauth2_scheme
 
 router = APIRouter(prefix="/users", tags=["users"])
 
@@ -18,10 +21,30 @@ async def get_profile(username: str):
         created_at=existing_user["created_at"],
     )
 
-@router.put("/{username")
-async def update_profile():
+@router.put("/{username}")
+async def update_profile(username: str, user_data: UserUpdate, current_user: str = Depends(get_current_user)):
+    if current_user != username:
+        raise HTTPException(status_code=403, detail="Sem permissão.")
 
-#
+    update_fields = {k: v for k, v in user_data.model_dump().items() if v is not None}
+
+    if not update_fields:
+        raise HTTPException(status_code=400, detail="Nenhum campo para atualizar.")
+
+    await db["users"].update_one({"username": username}, {"$set": update_fields})
+
+    updated_user = await db["users"].find_one({"username": username})
+
+    return UserResponse(
+        username=updated_user["username"],
+        email=updated_user["email"],
+        followers=updated_user["followers"],
+        following=updated_user["following"],
+        favorite_genres=updated_user["favorite_genres"],
+        created_at=updated_user["created_at"],
+
+    )
+
 # @router.put()
 #
 # @router.delete()
