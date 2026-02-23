57aea6921f5ae1131cbcbb50fe637994fd705fba
Henrique Luza <henriqueluza@gmail.com>
2026-02-23 16:52:43 -0300

    Coloca ProtectedRoute na página Home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from "./pages/Login";
 import Landing from "./pages/Landing";
 import Register from "./pages/Register";
+import ProtectedRoute from "./components/ProtectedRoute.tsx";
+import Home from "./pages/Home";
 
 function App() {
 
@@ -10,6 +12,11 @@ function App() {
           <Routes>
               <Route path="/login" element={<Login />}/>
               <Route path="/register" element={<Register />}/>
+              <Route path="/home" element={
+                  <ProtectedRoute>
+                      <Home />
+                  </ProtectedRoute>
+              }/>
               <Route path="/" element={<Landing/>}/>
           </Routes>
       </BrowserRouter>
