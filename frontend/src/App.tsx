3aff2ee1ae87f7dc1749ad2cc7eaa82dbb8f451d
Henrique Luza <henriqueluza@gmail.com>
2026-02-23 16:16:06 -0300

    Adiciona rota para Landing Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from "./pages/Login";
+import Landing from "./pages/Landing";
 
 function App() {
 
   return (
       <BrowserRouter>
           <Routes>
-              <Route path="/" element={<Login />}/>
+              <Route path="/login" element={<Login />}/>
+              <Route path="/" element={<Landing/>}/>
           </Routes>
       </BrowserRouter>
   )
