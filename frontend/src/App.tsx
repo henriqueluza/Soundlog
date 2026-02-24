6ed968bee5faa22d52c163a9b396a967fd1dab71
Henrique Luza <henriqueluza@gmail.com>
2026-02-24 14:00:28 -0300

    Envolve todo o app no AuthProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,22 +4,26 @@ import Landing from "./pages/Landing";
 import Register from "./pages/Register";
 import ProtectedRoute from "./components/ProtectedRoute.tsx";
 import Home from "./pages/Home";
+import {AuthProvider} from "./contexts/AuthContext.tsx";
 
 function App() {
 
   return (
-      <BrowserRouter>
-          <Routes>
-              <Route path="/login" element={<Login />}/>
-              <Route path="/register" element={<Register />}/>
-              <Route path="/home" element={
-                  <ProtectedRoute>
-                      <Home />
-                  </ProtectedRoute>
-              }/>
-              <Route path="/" element={<Landing/>}/>
-          </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+          <BrowserRouter>
+              <Routes>
+                  <Route path="/login" element={<Login />}/>
+                  <Route path="/register" element={<Register />}/>
+                  <Route path="/home" element={
+                      <ProtectedRoute>
+                          <Home />
+                      </ProtectedRoute>
+                  }/>
+                  <Route path="/" element={<Landing/>}/>
+              </Routes>
+          </BrowserRouter>
+      </AuthProvider>
+
   )
 }
 
