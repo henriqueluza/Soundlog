7296bfe10ebdba5335e0c5ffcdb2ea54acf5f564
Henrique Luza <henriqueluza@gmail.com>
2026-02-24 15:40:18 -0300

    Cria o componente react do perfil e o useEffect dentro dele

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,1 +1,29 @@
-    
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+import api from "../services/api";
+import type {UserPublicResponse} from "../services/types.ts";
+
+
+export default function Profile() {
+
+    const {username} = useParams()
+    const {user} = useAuth()
+    const [profile, setProfile] = useState<UserPublicResponse | null>(null)
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        api.get(`/${username}/profile`).then((response) => {
+            setProfile(response.data)
+        }).catch(() => {
+            setProfile(null)
+        }
+        ).finally(() => {
+                setIsLoading(false)
+            }
+        );
+    }, [username])
+    return (
+
+    )
+}
