321c8b98b39c0673930e5398276075ec2a1ec04b
Henrique Luza <henriqueluza@gmail.com>
2026-03-10 17:22:57 -0300

    Ajusta formatação do config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,19 +3,19 @@ from pathlib import Path
 
 class Settings(BaseSettings):
     SECRET_KEY: str
-    ALGORITHM : str = 'HS256'
-    ACCESS_TOKEN_EXPIRES : int = 7
+    ALGORITHM: str = 'HS256'
+    ACCESS_TOKEN_EXPIRES: int = 7
 
-    MONGO_URL : str
-    MONGO_DB_NAME : str = 'soundlog'
-    MONGO_USERNAME : str
-    MONGO_PASSWORD : str
+    MONGO_URL: str
+    MONGO_DB_NAME: str = 'soundlog'
+    MONGO_USERNAME: str
+    MONGO_PASSWORD: str
 
     REDIS_URL : str = 'redis://localhost:6379'
 
     # Spotify API
-    SPOTIFY_CLIENT_ID : str
-    SPOTIFY_CLIENT_SECRET : str
+    SPOTIFY_CLIENT_ID: str
+    SPOTIFY_CLIENT_SECRET: str
 
     model_config = {
         'env_file': Path(__file__).resolve().parents[3] / '.env',
