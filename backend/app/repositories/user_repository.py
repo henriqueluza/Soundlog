6e91f695e57e96757ad7b7ebef9510865dd46a9c
Henrique Luza <henriqueluza@gmail.com>
2026-03-12 15:18:53 -0300

    Implementa user_repository.py

---
+++ backend/app/repositories/user_repository.py
@@ -0,0 +1,14 @@
+from app.db.mongodb import get_db
+
+async def find_by_username(username: str): # verifica se já existe user com o mesmo username na hora de register
+    db = get_db()
+    return await db["users"].find_one({"username": username})
+
+async def find_by_email_or_username(login: str):
+    db = get_db()
+    return await db["users"].find_one({"$or": [{"username": login}, {"email": login}]})
+
+async def create_user(user_data: dict):
+    db = get_db()
+    return await db["users"].insert_one(user_data)
+
